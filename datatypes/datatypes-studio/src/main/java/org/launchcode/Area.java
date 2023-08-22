c7505e9b1ee11c09e022ca9fb4bd7db02abda227
Ariel <parnell.ariel@yahoo.com>
2023-08-21 20:07:46 -0500

    added conditional if statements to Area

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -5,9 +5,18 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the radius: ");
-        double radiusEquals = input.nextDouble();
-        //double area = 3.14 * radiusEquals * radiusEquals;
-        System.out.println("The area is: " + Circle.getArea(radiusEquals));
+        double radius = input.nextDouble();
+        double area = Circle.getArea(radius);
+
+        if (radius < 0) {
+            System.out.println("Error, must be a positive number");
+            return;
+        } else if (input.equals(" ")) {
+            System.out.println("Error, must enter something");
+            return;
+            
+        }
+        System.out.println("The area is: " + area);
 
         input.close();
 
