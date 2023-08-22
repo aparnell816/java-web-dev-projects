bfea268b3693a9c031d1dd44e26d7e0d38282010
Ariel <parnell.ariel@yahoo.com>
2023-08-21 19:39:26 -0500

    added org package and Area and Circle java classes

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        double radiusEquals = input.nextDouble();
+        double area = 3.14 * radiusEquals * radiusEquals;
+        System.out.println("The area is: " + area);
+
+        input.close();
+
+    }
+}
