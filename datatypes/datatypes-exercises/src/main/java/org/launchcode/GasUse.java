fa5699e868184d4ab5cea44fc796671075bc3560
Ariel <parnell.ariel@yahoo.com>
2023-08-23 03:09:54 -0500

    added method in GasUse to calculate mpg

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/GasUse.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/GasUse.java
@@ -1,7 +1,15 @@
 package org.launchcode;
+import java.util.Scanner;
 
 public class GasUse {
     public static void main(String[] args) {
-
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many miles have you driven? ");
+    double miles = input.nextDouble();
+    System.out.println("How much gas have you consumed (in gallons)? ");
+    double gasUsed = input.nextDouble();
+    input.close();
+    double mpg = miles/gasUsed;
+    System.out.println("You got " + mpg + " miles per gallon");
     }
 }
