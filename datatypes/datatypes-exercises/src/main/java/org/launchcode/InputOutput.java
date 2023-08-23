36900ee2c448d4d9e6b88af3301760776e6a457f
Ariel <parnell.ariel@yahoo.com>
2023-08-23 02:57:14 -0500

    closed input in InputOutput

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/InputOutput.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/InputOutput.java
@@ -6,6 +6,8 @@ public class InputOutput {
     Scanner input = new Scanner(System.in);
     System.out.println("Hello, what is your name?: ");
     String name = input.nextLine();
+    input.close();
     System.out.println("Hello " + name);
+
     }
 }
